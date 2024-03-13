434fd434c56825165c2483e2cdfa33d8668a6382
Juan Esteban Rodas <esteban.rodas@perficient.com>
2024-03-12 19:26:53 -0500

    Class 8 Stacks

--- StacksClass8/src/Main.java
+++ StacksClass8/src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Stack<Integer> stack = new Stack<Integer>();
+
+        //Display stack
+        System.out.println("Stack empty: " + stack);
+        System.out.println("Stack empty ? " + stack.isEmpty());
+
+        //Add element to the stack
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        for (Integer stacky: stack) {
+            System.out.println(stacky);
+        }
+
+        //display stack
+        System.out.println("Stack empty: " + stack);
+        System.out.println("Stack empty ? " + stack.isEmpty());
+
+        //Delete las item in the stack
+        stack.pop();
+        //Search item in the stack
+        System.out.println("is 3 there ? " + stack.search(3));
+        // Return the item in the top of the stack
+        System.out.println("last added: " + stack.peek());
     }
 }
